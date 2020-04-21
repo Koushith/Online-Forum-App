a26f9b0779643cbc0a65161a7ff248414cf24354
koushith <koushith97@gmail.com>
2020-04-21 19:13:01 +0530

    fixed middleware

--- models/article.js
+++ models/article.js
@@ -1,17 +1,13 @@
 const mongoose = require("mongoose");
-const marked = require("marked");
-const slugify = require("slugify");
-const createDomPurify = require("dompurify");
-const { JSDOM } = require("jsdom");
-const dompurify = createDomPurify(new JSDOM().window);
 
 const articleSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true
   },
-  description: {
-    type: String
+  title: {
+    type: String,
+    required: true
   },
   markdown: {
     type: String,
@@ -19,29 +15,8 @@ const articleSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  },
-  slug: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  sanitizedHtml: {
-    type: String,
-    required: true
+    default: Date.now()
   }
-});
-
-articleSchema.pre("validate", function(next) {
-  if (this.title) {
-    this.slug = slugify(this.title, { lower: true, strict: true });
-  }
-
-  if (this.markdown) {
-    this.sanitizedHtml = dompurify.sanitize(marked(this.markdown));
-  }
-
-  next();
 });
 
 module.exports = mongoose.model("Article", articleSchema);
--- server.js
+++ server.js
@@ -1,25 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Article = require("./models/article");
 const articleRouter = require("./routes/articles");
-const methodOverride = require("method-override");
 const app = express();
 
 mongoose.connect("mongodb://localhost/blog", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
+  useUnifiedTopology: true
 });
+
+// middleware config
 
 app.set("view engine", "ejs");
+// user article router
+app.use("/articles", articleRouter); //loc-its in articles.js file
+// access all the different options in the form.. body parcer
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
-
-app.get("/", async (req, res) => {
-  const articles = await Article.find().sort({ createdAt: "desc" });
-  res.render("articles/index", { articles: articles });
+app.get("/", (req, res) => {
+  const articles = [
+    {
+      title: "Post 1",
+      createdAt: new Date(),
+      description: "Test Description"
+    },
+    {
+      title: "Post 2",
+      createdAt: new Date(),
+      description: "Test Description"
+    }
+  ];
+  res.render("articles/index", {
+    articles: articles
+  });
 });
 
-app.use("/articles", articleRouter);
-
 app.listen(5000);
+console.log("Server is listening on Port 5000");
