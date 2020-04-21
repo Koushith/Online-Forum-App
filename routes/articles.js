fbea5fd0ad722bdbd64cc4197e1e1721767ed550
koushith <koushith97@gmail.com>
2020-04-21 14:05:53 +0530

    final

--- routes/articles.js
+++ routes/articles.js
@@ -1,14 +1,14 @@
-// this route contains all the article relates stuff
-
 const express = require("express");
-// imports model
-const Article = require("../models/article");
-// const Article = require("../models/article");
+const Article = require("./../models/article");
 const router = express.Router();
 
-// route for new articles
 router.get("/new", (req, res) => {
   res.render("articles/new", { article: new Article() });
+});
+
+router.get("/edit/:id", async (req, res) => {
+  const article = await Article.findById(req.params.id);
+  res.render("articles/edit", { article: article });
 });
 
 router.get("/:slug", async (req, res) => {
@@ -17,21 +17,42 @@ router.get("/:slug", async (req, res) => {
   res.render("articles/show", { article: article });
 });
 
-// on form submit this function will be called
-router.post("/", async (req, res) => {
-  let article = new Article({
-    // we are using the form fields, also from model
-    title: req.body.title,
-    description: req.body.description,
-    markedown: req.body.markdown
-  });
-  try {
-    article = await article.save();
-    res.redirect(`/articles/${article.id}`);
-  } catch (e) {
-    console.log(e);
-    res.render("/articles/new", { article: article });
-  }
+router.post(
+  "/",
+  async (req, res, next) => {
+    req.article = new Article();
+    next();
+  },
+  saveArticleAndRedirect("new")
+);
+
+router.put(
+  "/:id",
+  async (req, res, next) => {
+    req.article = await Article.findById(req.params.id);
+    next();
+  },
+  saveArticleAndRedirect("edit")
+);
+
+router.delete("/:id", async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id);
+  res.redirect("/");
 });
+
+function saveArticleAndRedirect(path) {
+  return async (req, res) => {
+    let article = req.article;
+    article.title = req.body.title;
+    article.description = req.body.description;
+    article.markdown = req.body.markdown;
+    try {
+      article = await article.save();
+      res.redirect(`/articles/${article.slug}`);
+    } catch (e) {
+      res.render(`articles/${path}`, { article: article });
+    }
+  };
+}
 
 module.exports = router;
