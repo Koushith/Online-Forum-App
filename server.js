82496ae9a5b26b7ddd53e2095a1b64c9cf79cfe9
koushith <koushith97@gmail.com>
2020-04-23 00:57:25 +0530

    Final Touches

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 // model
-const Article = require("./models/article");
-const articleRouter = require("./routes/articles");
+const Article = require('./models/article');
+const articleRouter = require('./routes/articles');
 // method ovrride
-const methodOverride = require("method-override");
+const methodOverride = require('method-override');
+const path = require('path');
 const app = express();
 
-mongoose.connect("mongodb://localhost/blogs", {
+mongoose.connect('mongodb://localhost/blogs', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndexes: true //errors thrown in the console
@@ -15,23 +16,26 @@ mongoose.connect("mongodb://localhost/blogs", {
 
 // middleware config
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
+
+// static files
+app.use(express.static(path.join(__dirname, 'views')));
 
 // access all the different options in the form.. body parcer
 app.use(express.urlencoded({ extended: false }));
 
 // override
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   const articles = await Article.find().sort({
-    createdAt: "desc"
+    createdAt: 'desc'
   }); //this will get every single article
-  res.render("articles/index", {
+  res.render('articles/index', {
     articles: articles
   });
 });
 // user article router
-app.use("/articles", articleRouter); //loc-its in articles.js file
+app.use('/articles', articleRouter); //loc-its in articles.js file
 app.listen(5000);
-console.log("Server is listening on Port 5000");
+console.log('Server is listening on Port 5000');
